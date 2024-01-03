863f4ea615269c4e7a669c71e8ed48d5a436169f
Iyanu <tobiy23@gmail.com>
2024-01-03 23:19:28 +0100

    remove char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    if n < 0 or n > len(str):
-        print("Invalid Offset")
-        return 0
     first = str[:n]
     second = str[n+1:]
     result = first + second
