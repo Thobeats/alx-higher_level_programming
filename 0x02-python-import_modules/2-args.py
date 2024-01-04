4faa8d5e419fdfe4c4aa6eef16710c8fd675170f
Iyanu <tobiy23@gmail.com>
2024-01-04 22:29:09 +0100

    import args module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    args_count == len(argv) - 1
+    args_count = len(argv) - 1
     arg_message = 'arguement'
     if args_count > 1:
         arg_message = arg_message + 's'
     if args_count > 0:
         print(f"{args_count} {arg_message}:")
-        for i in argv:
-            print(f"{i + 1} {argv[i]}")
+        for i, j in enumerate(argv):
+            if i == 0:
+                continue
+            print(f"{i} {j}")
     else:
         print(f"{args_count} {arg_message}.")
