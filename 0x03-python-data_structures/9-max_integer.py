b5b626044012c4c53171cbb288ace149b3dd8bd7
Iyanu <tobiy23@gmail.com>
2024-01-09 00:33:04 +0100

    matrix integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    my_list.sort()
-    return my_list[0]
+    first_char = my_list[0]
+    for i in my_list:
+        if i > first_char:
+            first_char = i
+    return first_char
