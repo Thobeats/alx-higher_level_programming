eebf602495ea11c9bb1c0705356aab6e0c31e624
Iyanu <tobiy23@gmail.com>
2024-01-08 23:54:13 +0100

    print matrix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     newString = ''
     for i in range(len(my_string)):
         if my_string[i] != 'C' and my_string[i] != 'c':
-            newString += my_string
+            newString += my_string[i]
     return newString
