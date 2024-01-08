7590b596c57014d259d559de31d2e15e15f7f958
Iyanu <tobiy23@gmail.com>
2024-01-08 22:58:00 +0100

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    newString = ''
+    for i in range(len(my_string)):
+        if my_string[i] != 'C' and my_string[i] != 'c':
+            newString += my_string
+    return newString
