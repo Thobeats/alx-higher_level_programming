bfcc183f6f380547e180600a03bf481768e641f6
Iyanu <tobiy23@gmail.com>
2024-01-10 22:30:15 +0100

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list_copy = my_list.copy()
+    for j, i in enumerate(list_copy):
+        if i == search:
+            list_copy[j] = replace
+    return list_copy
+            
