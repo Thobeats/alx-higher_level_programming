d810c676674ba2f357bd3cd71202f95f65ab564e
Iyanu <tobiy23@gmail.com>
2024-01-10 01:48:32 +0100

    palindrom

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,8 +9,8 @@ void print_python_list_info(PyObject *p)
 {
 	size_t len = PyList_Size(p);
 
-	printf("[*] Size of the Python list = %d\n", len);
-	printf("[*] Allocated = %d\n", object->allocated);
+	printf("[*] Size of the Python list = %ld\n", len);
+	printf("[*] Allocated = %ld\n", p->allocated);
 	for (i = 0, i < len; i++)
 		printf("Element %d: %s\n", i, "str");
 }
