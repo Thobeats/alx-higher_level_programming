ac2bb4084279a070b9c799178a3aa5cc114b618d
Iyanu <tobiy23@gmail.com>
2024-01-24 02:34:37 +0100

    update square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,5 @@
 #!/bin/usr/python3
-"""
-This module defines a Square class.
-Usage:
-    square = Square(5)
-    area = square.calculate_area()
-    perimeter = square.calculate_perimeter()
-"""
+"""This module defines a Square class."""
 class Square:
+    """defines empty class."""
     pass
