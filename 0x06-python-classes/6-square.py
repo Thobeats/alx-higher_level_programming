06f2207987be20a5b8ab3ec62ce6c21e64fe1436
Iyanu <tobiy23@gmail.com>
2024-01-25 01:37:45 +0100

    update square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,6 +24,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        self.__position = position
 
     def area(self):
         """
@@ -59,6 +60,29 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """
+        Retrieves the position coordinates
+
+        Returns:
+        - tuple: The position coordinates
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Sets the position coordinates
+
+        Parameters:
+        - value (tuple): The new coordinates
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
+            raise TypeError("Tuple must be a tuple of two positive integers")
+
+        self.__position = value
+
     def my_print(self):
         """
         Prints the value of the square using #
@@ -67,7 +91,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
                 for i in range(self.__size):
                     print("#", end="")
                 print()
