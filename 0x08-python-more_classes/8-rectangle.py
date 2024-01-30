dec8344b14a36f8dd4976c2e25d768b65882d01a
Iyanu <tobiy23@gmail.com>
2024-01-30 04:55:09 +0100

    add static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -144,6 +144,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not rect_2 or not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() == rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        return max([rect_1.area(), rect_2.area()])
+        else:
+            return rect_2
