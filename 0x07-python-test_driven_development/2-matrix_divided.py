c1821a89bb5248a0820de91111eff1c842b4b8ba
Iyanu <tobiy23@gmail.com>
2024-01-31 08:56:18 +0100

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """ Divides the values in a matrix """
+
+
 def matrix_divided(matrix, div):
     """ checks for the state of the arguments """
+
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
     if (div == 0):
         raise ZeroDivisionError("division by zero")
-    if not all(isinstance(i, list) and all(isinstance(j, (int, float)) for j in i) for i in matrix):
+    if not all(isinstance(i, list) 
+            and all(isinstance(j, (int, float)) for j in i) 
+            for i in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     row_sizes = set(len(row) for row in matrix)
     if (len(row_sizes)) > 1:
