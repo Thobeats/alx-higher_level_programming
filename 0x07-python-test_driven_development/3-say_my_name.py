5074e8321ca9b418ee759c4c7b65c3208410d38c
Iyanu <tobiy23@gmail.com>
2024-01-31 09:22:03 +0100

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" Prints out the firstname and the lastname """
+
+
 def say_my_name(first_name, last_name=""):
+    """ checks for the firstname and the lastname """
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
