4d853772436b75e0a94bca716983c3db4d63347a
Iyanu <tobiy23@gmail.com>
2024-01-31 05:19:11 +0100

    matrix handler

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+    if (div == 0):
+        raise ZeroDivisionError("division by 0")
+    if not all(isinstance(i, list) and all(isinstance(j , (int, float)) for j in i) for i in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    row_sizes = set(len(row) for row in matrix)
+    if (len(row_sizes)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    newMatrix = list(map (lambda ir: list(map(lambda val: round(val/div, 2), ir)), matrix))
+    return newMatrix
