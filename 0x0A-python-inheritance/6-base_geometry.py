04e4e1fa82b28f5cc020b2c4442b46c7a6bfc0d1
Iyanu <tobiy23@gmail.com>
2024-02-06 04:43:44 +0100

    base geometry exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""create an empty BaseGeometry class"""
+
+
+class BaseGeometry():
+    """creates an empty class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+
