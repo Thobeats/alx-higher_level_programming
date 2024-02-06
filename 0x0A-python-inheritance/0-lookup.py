1da7b267f43bc87ee28ed66fe513061504fd00d8
Iyanu <tobiy23@gmail.com>
2024-02-06 03:47:02 +0100

    Lookup an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns all the available methods and attributes in an object"""
+
+
+def lookup(obj):
+    """returns a list of the attributes and methods"""
+
+    return dir(obj)
