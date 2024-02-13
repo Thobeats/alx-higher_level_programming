a922e478baa3d5dd13d01d57d289cdff154d8b92
Iyanu Dayisi <tobiy23@gmail.com>
2024-02-13 07:41:26 +0100

    update tests and base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,10 +3,11 @@ import unittest
 import pep8
 
 from models.base import Base
+from models.rectangle import Rectangle
 
 class TestBase(unittest.TestCase):
     
-    def test_base_instantiation(self):
+    def test__init__(self):
         """
         different tests for the id property
         test for id is None and id has a value
@@ -25,6 +26,16 @@ class TestBase(unittest.TestCase):
 
         b5 = Base()
         self.assertEqual(b5.id, 4)
+    
+    def test_to_json_string(self):
+        """
+        test the to json string function in the Base Class
+        """
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertDictEqual(dictionary, {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8})
+        self.assertEqual(json_dictionary, '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]')
 
     def test_pepEight_code_style(self):
         """ test if the code follows pep8 codestyle """
