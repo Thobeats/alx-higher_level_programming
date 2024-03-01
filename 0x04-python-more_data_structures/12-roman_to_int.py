98d1f7c33d295fb2d0afca63a8ab3c0d1da81548
Iyanu Dayisi <tobiy23@gmail.com>
2024-03-01 13:43:41 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not (roman_string):
         return 0
-    if isinstance(roman_string, int) == True:
+    if isinstance(roman_string, int) is True:
         return 0
     value = 0
     convert = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
