ae7d327a9c91a7a4a8b5eb104e4b5a3838e1a64f
Iyanu Dayisi <tobiy23@gmail.com>
2024-03-02 16:13:45 +0100

    more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -126,14 +126,7 @@ class TestRectangle(unittest.TestCase):
 
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 1/3 - 4/2")
-    
-    def test_rectangle_display():
-        """
-        test the display function
-        """
-        
 
-    
     def test_pepEight_code_style(self):
         """ test if the code follows pep8 codestyle """
         pepEightStyle = pep8.StyleGuide(quiet='true')
