9e03ccd895ef1dc0627621a48a67bbbd2b98e4f8
Iyanu Dayisi <tobiy23@gmail.com>
2024-03-02 16:46:30 +0100

    more and more and more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,9 +6,17 @@ import unittest
 import pep8
 
 from models.rectangle import Rectangle
+from io import StringIO
+from unittest.mock import patch
 
 class TestRectangle(unittest.TestCase):
     """ tests for the rectangle class """
+
+    def setUp(self):
+        """
+        Instantiate a new rectangle
+        """
+        self.rect = Rectangle(5, 3)
 
     def test_rectangle_instantiation(self):
         """
@@ -35,21 +43,18 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.height, 14)
         self.assertEqual(r2.x, 0)
         self.assertEqual(r2.y, 0)
-        self.assertEqual(r2.id, 6)
 
         r3 = Rectangle(12, 15, 5)
         self.assertEqual(r3.width, 12)
         self.assertEqual(r3.height, 15)
         self.assertEqual(r3.x, 5)
         self.assertEqual(r3.y, 0)
-        self.assertEqual(r3.id, 7)
 
         r4 = Rectangle(12, 15, 5, 6)
         self.assertEqual(r4.width, 12)
         self.assertEqual(r4.height, 15)
         self.assertEqual(r4.x, 5)
         self.assertEqual(r4.y, 6)
-        self.assertEqual(r4.id, 8)
 
         with self.assertRaises(TypeError):
             r5 = Rectangle("12", 15)
@@ -86,7 +91,6 @@ class TestRectangle(unittest.TestCase):
         """
             test the area of the rectangle instance
         """
-
         r2 = Rectangle(20, 10)
         area = r2.area()
         self.assertEqual(area, 200)
@@ -96,9 +100,8 @@ class TestRectangle(unittest.TestCase):
         test the update function of the rectangle
         class
         """
-
         r1 = Rectangle(10, 10, 10, 10)
-        self.assertEqual(r1.__str__(), "[Rectangle] (19) 10/10 - 10/10")
+        self.assertEqual(r1.__str__(), "[Rectangle] (25) 10/10 - 10/10")
 
         r1.update(89)
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 10/10")
@@ -126,6 +129,16 @@ class TestRectangle(unittest.TestCase):
 
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 1/3 - 4/2")
+    
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_rectangle_display(self, mock_stdout):
+        """
+        test the display function
+        """
+        expected_output = "#####\n#####\n#####\n"
+        expected_output2 = "  #####\n  #####\n  #####\n"
+        self.rect.display()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
 
     def test_pepEight_code_style(self):
         """ test if the code follows pep8 codestyle """
