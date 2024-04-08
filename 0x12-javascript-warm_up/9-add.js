ad3256402f3acb275cba478b77067445b674ad02
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-09 00:44:23 +0100

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+function add(a, b) {
+  return (parseInt(a) + parseInt(b));
+}
 const [, , first, second] = process.argv;
 if (isNaN(first) || isNaN(second)) {
   console.log('NaN');
 } else {
-  console.log(first + second);
+  console.log(add(first, second));
 }
