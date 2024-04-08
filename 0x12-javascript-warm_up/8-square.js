bdd0452b3142f49e298f620c897e22b0ada84caf
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-09 00:12:29 +0100

    multi language loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const [, , occurence] = process.argv;
+if (isNaN(occurence)) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  let j = 0;
+  let mySquare = '';
+  
+  while (j < occurence) {
+    mySquare += 'X';
+    j++;
+  }
+
+  while (i < occurence) {
+    console.log(mySquare);
+    i++;
+  }
+}
