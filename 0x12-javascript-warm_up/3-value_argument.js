cd15897599b9059b6335f37698268ef93d055221
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-08 23:25:53 +0100

    value argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.length - 2;
-if (args === 0) {
+const myArgs = process.argv.slice(2);
+if (myArgs[0]) {
+  console.log(myArgs[0]);
+} else {
   console.log('No argument');
-} else if (args > 0) {
-  const response = process.argv.slice(2);
-  console.log(response[0]);
 }
