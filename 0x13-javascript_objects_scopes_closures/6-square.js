d00ea2614a94d8128f7ead98129e14a25d28eb2f
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-10 00:09:07 +0100

    char Print

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    let myX = '';
+    let i = 0;
+    let j = 0;
+    while (j < this.width) {
+      myX += c;
+      j++;
+    }
+
+    while (i < this.height) {
+      console.log(myX);
+      i++;
+    }
+  }
+}
+
+module.exports = Square;
