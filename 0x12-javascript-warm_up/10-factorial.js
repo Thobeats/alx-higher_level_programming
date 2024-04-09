0f47fa6fa738963b540b0f38f8ca24114d5ec0ba
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-09 01:02:59 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (a >= 0) {
+    return (parseInt(a) + parseInt(factorial(a - 1)));
+  }
+  return 0;
+}
+const [, , factorialNum] = process.argv;
+if (isNaN(factorialNum)) {
+  console.log(1);
+} else {
+  console.log(factorial(factorialNum));
+}
