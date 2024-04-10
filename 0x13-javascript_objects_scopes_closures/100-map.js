dde07adaa8edd6ce394e3966a407f88732c65d16
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-10 01:57:09 +0100

    map function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const list = require('./100-data');
-const list2 = list.map((lt, index)=>{
+
+const list2 = list.list.map((lt, index) => {
   return lt * index;
 });
 
-console.log(list);
+console.log(list.list);
 console.log(list2);
