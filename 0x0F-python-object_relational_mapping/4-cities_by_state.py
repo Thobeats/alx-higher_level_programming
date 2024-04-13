a513477b2133937316df1b7a68e3db426c5daf23
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-13 13:03:04 +0100

    select all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all cities
+from the database hbtn_0e_4_usa
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2], db=argv[3],
+        charset="utf8"
+        )
+    cur = conn.cursor()
+    sql = """
+            SELECT cities.id, states.name, cities.name
+            FROM cities
+            INNER JOIN states
+            ON cities.state_id = state.id
+            ORDER BY cities.id ASC
+            """
+    value = (argv[4],)
+    cur.execute(sql.format(argv[3]), value)
+    cities = cur.fetchall()
+
+    if cities is not None:
+        for city in cities:
+            print(city)
+
+    cur.close()
+    conn.close()
