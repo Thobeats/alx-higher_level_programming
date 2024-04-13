d48a001981509c853bf234aee31dc6b61c6b217a
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-13 15:35:11 +0100

    create the state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a python file that
+contains the class definition
+of a State and an instance
+Base = declarative_base()
+"""
+
+from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    define a State class that
+    connects to the mysql states table
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                unique=True,
+                autoincrement=True
+                )
+    name = Column(String(128), nullable=False)
