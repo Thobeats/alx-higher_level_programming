f3d00e440ed93896c18f23bacab78e096e9cd99d
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-13 13:11:52 +0100

    cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             SELECT cities.id, states.name, cities.name
             FROM cities
             INNER JOIN states
-            ON cities.state_id = state.id
+            ON cities.state_id = states.id
             ORDER BY cities.id ASC
             """
     cur.execute(sql)
