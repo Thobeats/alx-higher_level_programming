27ca3f17ba0fb1325428ccba7dc62af5484195c6
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-15 11:40:42 +0100

    city and state relationship

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """
-Write a script that
-lists all State objects,
-and corresponding City objects,
-contained in the database
-hbtn_0e_101_usa
+Write a script that lists
+all City objects from the
+database hbtn_0e_101_usa
 """
 
 from sys import argv
@@ -24,9 +22,6 @@ if __name__ == "__main__":
     session = Session()
     Base.metadata.create_all(engine)
 
-    for state in session.query(State). \
-            order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+    for city in session.query(City).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
     session.close()
