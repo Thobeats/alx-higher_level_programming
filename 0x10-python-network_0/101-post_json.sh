5c835b098ed36a1a5f6a9cf0e7e6b223fee976b2
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 00:59:31 +0100

    post json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -H "Content-Type: application/json" -X POST -sd "$2" "$1"
+curl -H "Content-Type: application/json" -X POST -sF "data=$2" "$1"
