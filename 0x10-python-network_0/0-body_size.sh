9e5a5f4ae0f082d0a1e89236c5f8ce46e15bb6e7
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-25 13:05:39 +0100

    get the body size of a url

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/bash
-# Write a Bash script that takes in a URL,
-# sends a request to that URL, and displays
-# the size of the body of the response
-if [ $# == 1 ];
-then
-    URL=$1
-    SIZE_RESPONSE=$(curl -sI "$URL" | grep "Content-Length" | awk '{print $2}')
-    echo "$SIZE_RESPONSE"
-fi
+# Write a Bash script that takes in a URL,and the size of the body of the response
+echo $(curl -sI "$1" | grep "Content-Length" | awk '{print $2}')
