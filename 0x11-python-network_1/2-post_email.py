b530fe14c32b7dd9b396cc5d06b44010641f75c6
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 15:55:19 +0100

    make a post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+if __name__ == "__main__":
+    import urllib.request as urlreq
+    import urllib.parse as urlparse
+    import sys
+    url = sys.argv[1]
+    data = {
+        "email": sys.argv[2]
+    }
+    data = urlparse(data).encode('utf8')
+    # Make a GET request
+    with urlreq.urlopen(url, data) as response:
+        result = response.read()
+        print(result.decode())
