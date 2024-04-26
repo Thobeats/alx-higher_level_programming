119bc91395f583c4b23905f66910c17e16cd4683
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 16:42:11 +0100

    send a post request with requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    data = {
+        "email": sys.argv[2]
+    }
+    result = requests.post(url, data)
+    print(result.decode())
