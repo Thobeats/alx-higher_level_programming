270db447048d657d6d68ebdbc42f25aa42dbd309
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 16:26:33 +0100

    hbtn status using requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,13 +3,13 @@
 Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
-        import requests
-        url = 'https://alx-intranet.hbtn.io/status'
-        # Make a GET request
-        result = requests.get(url)
-        # resf means response format
-        resf = "Body response:\n"
-        resf += "\t- type: {}\n"
-        resf += "\t- content: {}"
-        resf = resf.format(type(result.text), result.text)
-        print(resf)
+    import requests
+    url = 'https://alx-intranet.hbtn.io/status'
+    # Make a GET request
+    result = requests.get(url)
+    # resf means response format
+    resf = "Body response:\n"
+    resf += "\t- type: {}\n"
+    resf += "\t- content: {}"
+    resf = resf.format(type(result.text), result.text)
+    print(resf)
