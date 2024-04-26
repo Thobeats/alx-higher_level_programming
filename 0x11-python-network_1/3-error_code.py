7e0c54c0079251aa3a836365f058fe828e984145
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 16:10:09 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+if __name__ == "__main__":
+    import urllib.request as urlreq
+    import urllib.parse as urlparse
+    from urllib.error import HTTPError
+    import sys
+    url = sys.argv[1]
+    data = {
+        "email": sys.argv[2]
+    }
+    data = urlparse.urlencode(data).encode('utf8')
+    try:
+        # Make a GET request
+        with urlreq.urlopen(url, data) as response:
+            result = response.read()
+            print(result.decode())
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
