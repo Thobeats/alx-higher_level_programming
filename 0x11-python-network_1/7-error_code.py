c969e6dc951a4dd5d0e6dd2d12ffd71b0f622045
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 16:53:55 +0100

    handle error exception

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     result = requests.get(url)
     if result.status_code >= 400:
-        print(result.status_code)
+        print("Error code: {}".format(result.status_code))
     else:
         print(result.text)
