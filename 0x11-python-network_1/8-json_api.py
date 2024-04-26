683ff1347c20a34186206268555ef75543980f71
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-26 17:10:14 +0100

    handle json results

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a letter
+and sends a POST request to
+http://0.0.0.0:5000/search_user with
+the letter as a parameter.
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {}
+    if (len(sys.argv) < 2):
+        data['q'] = ""
+    else:
+        data['q'] = sys.argv[1]
+    try:
+        result = requests.post(url, data)
+    except requests.exceptions.JSONDecodeError as e:
+        if e.status_code == 204:
+            print("No content")
+        else:
+            print("Not a valid JSON")
+    else:
+        print(result.json())
