1be508f8b224792d7422ab6a3540bbab9fad9e09
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-27 04:43:31 +0100

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer 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",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    result = requests.request('GET', url, headers=headers)
+    if result.status_code == 200:
+        resultJson = result.json()
+        i = 0
+        while i < 10:
+            print("{}: {}".format(resultJson[i]['sha'],
+                                  resultJson[i]['commit']['author']['name']))
+            i = i + 1
