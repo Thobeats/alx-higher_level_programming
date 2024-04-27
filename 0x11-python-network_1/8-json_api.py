fbe2cd6552f4ea4b31171aafe2f81ebaac405bd3
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-27 16:38:14 +0100

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a letter
-and sends a POST request to
-http://0.0.0.0:5000/search_user with
-the letter as a parameter.
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
 """
-if __name__ == "__main__":
-    import requests
-    import sys
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    q = argv[1] if len(argv) == 2 else ""
     url = 'http://0.0.0.0:5000/search_user'
-    data = {}
-    if (len(sys.argv) < 2):
-        data['q'] = ""
-    else:
-        data['q'] = sys.argv[1]
-    result = requests.post(url, data=data)
+    r = requests.post(url, data={'q': q})
     try:
-        res = result.json()
-    except (AttributeError):
-        print("Not a valid JSON")
-    else:
-        if (res == {}):
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
             print("No result")
         else:
-            print("[{}] {}".format(res['id'], res['name']))
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")
