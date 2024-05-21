f1bced144d2ed2021c1f7f1d691973bdb342f248
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-22 00:27:06 +0100

    get the starwars title from the starwars api using the request module

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const [,, movieId] = process.argv;
-const { response } = require('express');
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(url, (error, response, body) => {
-  bodyJson = JSON.parse(body)
-  console.log(bodyJson.title) 
+  if (error) {
+    return console.error(error + response);
+  }
+  const bodyJson = JSON.parse(body);
+  console.log(bodyJson.title);
 });
