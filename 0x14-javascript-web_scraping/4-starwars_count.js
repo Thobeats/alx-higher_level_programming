4e9bce5427805bcdd0713f85f36ee5bf21e8ed83
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-22 00:49:57 +0100

    count the starwars movie where Wedge Antillesis present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,23 @@
 #!/usr/bin/node
-const [,, url] = process.argv;
+// script that counts the number of films Wedge Antilles is in
 const request = require('request');
-
-request(url, (error, response, body) => {
-  if (error) {
-    return console.error(error + response);
+const myArgs = process.argv.slice(2);
+let result = 0;
+request(myArgs[0], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    let i = 0;
+    for (i = 0; i < results.length; i++) {
+      let j = 0;
+      const character = results[i].characters;
+      for (j = 0; j < character.length; j++) {
+        if (character[j].includes('18')) {
+          result += 1;
+        }
+      }
+    }
+    console.log(result);
   }
-
-  const bodyJson = JSON.parse(body);
-  const movies = bodyJson.results.filter((record) => {
-    return record.characters.indexOf('https://swapi-api.alx-tools.com/api/people/18/') !== -1;
-  });
-  console.log(movies.length);
 });
