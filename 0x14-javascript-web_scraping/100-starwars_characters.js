1486256cd55944d5f12ba993b09bfce5199c5612
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-22 02:01:12 +0100

    star wars characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const [,, movieId] = process.argv;
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 const options = {
-  url: 'https://swapi-api.alx-tools.com/api/people/',
+  url: filmUrl,
   headers: {
     'User-Agent': 'request',
     Accept: 'application/json'
@@ -14,9 +15,17 @@ request(options, (error, response, body) => {
   if (error) {
     return console.error(error);
   }
-  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-  const films = JSON.parse(body).results.filter(person => person.films.includes(filmUrl));
-  for (const flm of films) {
-    console.log(flm.name);
+
+  let bodyJson = JSON.parse(body)
+  for (let character of bodyJson.characters) {
+    request(character, (error, response, res) => {
+      if (error) {
+        return console.error(error);
+      }
+
+      let characterBody = JSON.parse(res)
+      console.log(characterBody.name)
+    });
+    
   }
 });
