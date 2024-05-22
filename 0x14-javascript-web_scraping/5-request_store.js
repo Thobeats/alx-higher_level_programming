57e461703aea59dd9ced180f82506166377d27a6
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-22 01:33:17 +0100

    stream the results from a url into a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-// script that counts the number of films Wedge Antilles is in
 const request = require('request');
-const fs = require('fs')
+const fs = require('fs');
 const [,, url, filePath] = process.argv;
 
-request(url).pipe(fs.createWriteStream(filePath, { encoding: 'utf-8'}))
+request(url).pipe(fs.createWriteStream(filePath, { encoding: 'utf-8' }));
